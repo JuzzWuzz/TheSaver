bd69f3c2161e51479499f90418514e5874474c21
Rainer Dreyer <rdrey1@gmail.com>
2011-01-28 21:32:03 +0200

    should draw a pixel

--- JewSaver/Stickfigure.cs
+++ JewSaver/Stickfigure.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 class Stickfigure
 {
-    void draw() {;}
+    private Vector2 position;
+    private bool moving = false;
+
+    Stickfigure(Vector2 position)
+    {
+        this.position = position;
+    }
+
+    void draw() {    
+        VertexPositionColor[] points = {new VertexPositionColor(position, Color.Black)};
+
+
+        JewSaver.spriteBatch.GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(
+            PrimitiveType.PointList,
+            points,
+            0,
+            1);
+    }
 
     void update(){;}
 
