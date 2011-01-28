ed002988eeb6a6a513b32e277194f90555799fe4
Rainer Dreyer <rdrey1@gmail.com>
2011-01-28 20:01:16 +0200

    created stickfigure.cs

---
+++ JewSaver/Stickfigure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JewSaver
+{
+    class Stickfigure
+    {
+        void draw();
+
+        void update();
+
+
+    }
+}
