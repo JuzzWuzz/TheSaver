44741ad62d4a46e1869132743ae2fc5c12be5a22
Rainer Dreyer <rdrey1@gmail.com>
2011-01-28 20:33:10 +0200

    fixed namespace

--- JewSaver/Stickfigure.cs
+++ JewSaver/Stickfigure.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace JewSaver
+class Stickfigure
 {
-    class Stickfigure
-    {
-        void draw() {;}
+    void draw() {;}
 
-        void update(){;}
+    void update(){;}
 
 
-    }
 }
