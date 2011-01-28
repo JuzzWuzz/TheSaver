8801e71f54239761165cb249d16bef7150277643
Rainer Dreyer <rdrey1@gmail.com>
2011-01-28 23:18:28 +0200

    before merge

--- JewSaver/Stickfigure.cs
+++ JewSaver/Stickfigure.cs
@@ -9,15 +9,20 @@ class Stickfigure
 {
     private Vector2 position;
     private bool moving = false;
+
     private Vector2 crotch, shoulder, lHand, rHand, lFoot, rFoot;
     private float roShoulder, roLHand, roRHand, roLFoot, roRFoot;
 
-    //VertexPositionColor[] points;
+    VertexPositionColor[] points;
 
     public Stickfigure(Vector2 position)
     {
         this.position = position;
         roShoulder = roLHand = roRHand = roLFoot = roRFoot = 0f;
+
+        shoulder = new Vector2(0, -10);
+        lHand = rHand = new Vector2(5, 0);
+        lFoot = rFoot = new Vector2(2, 7);
 
         setLimbs();
     }
@@ -25,18 +30,29 @@ class Stickfigure
     private void setLimbs()
     {
         crotch = position;
-        //shoulder = position + Vector2.Dot(shoulder, Vector2
+        Vector2 shoulderPoint = crotch + shoulder;
+        Vector2 lHandPoint = shoulderPoint + lHand;
+        Vector2 rHandPoint = shoulderPoint + rHand;
+        Vector2 rFootPoint = crotch + rFoot;
+        Vector2 lFootPoint = crotch + lFoot;
+
+        points = new VertexPositionColor[10];
+        points[0] = new VertexPositionColor(new Vector3(crotch, 0f), Color.Red);
+        points[1] = new VertexPositionColor(new Vector3(shoulderPoint, 0f), Color.Yellow);
+        points[2] = new VertexPositionColor(new Vector3(crotch, 0f), Color.Red);
+        points[3] = new VertexPositionColor(new Vector3(lHandPoint, 0f), Color.Yellow);
+        points[0] = new VertexPositionColor(new Vector3(crotch, 0f), Color.Red);
+        points[1] = new VertexPositionColor(new Vector3(shoulderPoint, 0f), Color.Yellow);
+        
+
     }
 
     public void draw() {
 
-        VertexPositionColor[] points = {new VertexPositionColor(new Vector3(position, 0f), Color.Red),
-                                           new VertexPositionColor(new Vector3(position + new Vector2(0, 10), 0f), Color.Red)};
-
         JewSaver.spriteBatch.GraphicsDevice.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.LineList,
             points,
             0,
-            1);
+            3);
     }
 
     public void update(){
