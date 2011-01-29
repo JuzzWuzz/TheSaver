ed1ff90da6aa8ca8567789101a5073919723e7fe
Rainer Dreyer <rdrey1@gmail.com>
2011-01-29 18:42:34 +0200

    now adding locusts to level

--- JewSaver/LocustSwarm.cs
+++ JewSaver/LocustSwarm.cs
@@ -5,7 +5,7 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-class LocustSwarm
+public class LocustSwarm
 {
     static Random r = new Random();
     public bool active = true;
@@ -60,9 +60,9 @@ class LocustSwarm
     float timeOut = 0.95f;
     public void update(float dt)
     {
-        timeOut += dt * 200f;
+        timeOut += dt;
         timeToLive -= dt;
-        while (timeToLive > 0)
+        while (timeToLive > 0 && timeOut > .25)
             spawnLocust();
         foreach (Locust l in locusts)
             l.update(dt);
@@ -70,6 +70,7 @@ class LocustSwarm
 
     private void spawnLocust()
     {
+        timeOut = 0;
         int spawnZone = 192 / 3;
         switch (r.Next(100))
         {
@@ -83,7 +84,7 @@ class LocustSwarm
                 locusts.Add(new Locust(new Vector2(LevelBase.levelLength, r.Next(spawnZone * 2, spawnZone * 3)), this));
                 break;
         }
-        Console.WriteLine("Spawned at: " + locusts.Last<Locust>().pos.Y);
+        Console.WriteLine("Spawned at: " + locusts.Last<Locust>().pos.Y+"timeToLive: "+timeToLive);
 
     }
 }
