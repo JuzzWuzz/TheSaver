3bc59d966b0d62941734005a215bdc2a3bd6531d
Rizmari Versfeld <rizziepit@gmail.com>
2011-01-29 09:13:37 +0200

    first level added

---
+++ JewSaver/Level1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+public class Level1:LevelBase
+{
+    public Level1(JewSaver game)
+        : base(game, 1536)
+    {
+    }
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        AddCanyon(294, 294 + 256);
+        AddCanyon(960, 1280);
+        AddTrees();
+    }
+}
